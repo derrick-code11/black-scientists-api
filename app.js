892a66abdf9e563c15a733159d00e9a0393fb5c5
derrick-code11 <agyemangderrick777@gmail.com>
2023-08-04 00:44:51 -0400

    updated server

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const scientistsRouter = require("./routes/scientists");
 require("dotenv/config");
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 // connecting to MongoDB
 mongoose
